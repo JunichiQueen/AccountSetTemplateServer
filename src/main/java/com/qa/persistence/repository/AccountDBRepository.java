fdb5a84cebd3e7f7232e5637278172be209bf732
Harry <junichim@hotmail.com>
2019-06-06 08:52:58 +0100

    pushing all files

---
+++ src/main/java/com/qa/persistence/repository/AccountDBRepository.java
@@ -0,0 +1,57 @@
+package com.qa.persistence.repository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AccountDBRepository {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column
+	private String accountNumber;
+	@Column
+	private String firstName;
+	@Column
+	private String lastName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public AccountDBRepository() {
+
+	}
+
+}
---
+++ src/main/resources/META-INF/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO AccountDBRepository (accountNumber, firstName, lastName)
+VALUES (1234, John, Doe);
+INSERT INTO AccountDBRepository (accountNumber, firstName, lastName)
+VALUES (1235, Jane, Doe);
+INSERT INTO AccountDBRepository (accountNumber, firstName, lastName)
+VALUES (1236, Jim, Taylor);
